c5da62522ba05f3fa6e2cff5520c9cc9fb9d1318
dmanaster <david@manaster.com>
2019-11-15 14:57:46 -0500

    Adds scopes to order extensions alphabetically and by latest update.

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -2,4 +2,6 @@ class Extension < ApplicationRecord
   has_many :updates, :dependent => :destroy
   validates :name, :presence => true
   validates :url, :presence => true
+  scope :alphabetical, -> { order(name: :ASC) }
+  scope :last_updated, -> { includes(:updates).order('updates.date DESC') }
 end
