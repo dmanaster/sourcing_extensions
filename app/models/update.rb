25149f4a993597082fd752cb63e52da07d087c9f
dmanaster <david@manaster.com>
2019-12-10 21:48:08 -0500

    Fixes ordering issue.

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,6 +1,6 @@
 class Update < ApplicationRecord
   belongs_to :extension
   validates_uniqueness_of :version
-  scope :only_majors, -> { where(major: true) }
-  scope :unreviewed, -> { where(reviewed: false) }
+  scope :only_majors, -> { where(major: true).order(version: :DESC) }
+  scope :unreviewed, -> { where(reviewed: false).order(version: :DESC) }
 end
